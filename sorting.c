e2ce31a5aea709cc4d8c3234d5c556cf7c4d4932
devsungyeon <sungyeonlee1103@gmail.com>
2021-03-06 16:38:42 +0900

    merge quick done

--- sorting.c
+++ sorting.c
@@ -1,5 +1,65 @@
 #include <stdio.h>
 #define MAX_SIZE 10
+int sorted[MAX_SIZE];
+
+void selection(int arr[], int n);
+void insertion(int arr[], int n);
+void bubble(int arr[], int n);
+void merge(int arr[], int left, int right);
+void quick(int arr[], int start, int end);
+
+int main() {
+    int n = MAX_SIZE;
+    int list[MAX_SIZE];
+    int i;
+
+    for(i = 0 ; i < n ; i++ ) list[i] = n-i;
+    printf("selection before : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+    selection(list, n);
+    printf("selection after : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+
+    for(i = 0 ; i < n ; i++ ) list[i] = n-i;
+    printf("insertion before : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+    insertion(list, n);
+    printf("insertion after : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+
+    for(i = 0 ; i < n ; i++ ) list[i] = n-i;
+    printf("bubble before : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+    bubble(list, n);
+    printf("bubble after : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+    
+    for(i = 0 ; i < n ; i++ ) list[i] = n-i;
+    printf("merge before : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+    merge(list, 0, n-1);
+    printf("merge after : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+
+    for(i = 0 ; i < n ; i++ ) list[i] = n-i;
+    printf("quick before : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+    quick(list, 0, n-1);
+    printf("quick after : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+
+    return 0;
+}
 
 void selection(int arr[], int n) {
     int i, j, temp;
@@ -45,37 +105,65 @@ void bubble(int arr[], int n) {
     }
 }
 
-int main() {
-    int n = MAX_SIZE;
-    int list[MAX_SIZE];
-    int i;
+void merge_list(int arr[], int left, int mid, int right) {
+    int i, j, k, l;
+    i = left;
+    j = mid+1;
+    k = left;
 
-    for(i = 0 ; i < n ; i++ ) list[i] = n-i;
-    printf("selection before : ");
-    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
-    printf("\n");
-    selection(list, n);
-    printf("selection after : ");
-    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
-    printf("\n");
+    while(i<=mid && j<=right) {
+        if(arr[i] <= arr[j])
+            sorted[k++] = arr[i++];
+        else
+            sorted[k++] = arr[j++];
+    }
 
-    for(i = 0 ; i < n ; i++ ) list[i] = n-i;
-    printf("insertion before : ");
-    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
-    printf("\n");
-    insertion(list, n);
-    printf("insertion after : ");
-    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
-    printf("\n");
+    int tmp = i>mid ? j : i;
 
-    for(i = 0 ; i < n ; i++ ) list[i] = n-i;
-    printf("bubble before : ");
-    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
-    printf("\n");
-    bubble(list, n);
-    printf("bubble after : ");
-    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
-    printf("\n");
+    while(k<=right) sorted[k++] = arr[tmp++];
 
-    return 0;
+    for(l=left; l<=right; l++) arr[l] = sorted[l];
+}
+
+void merge(int arr[], int left, int right) {
+    int mid;
+    if(left < right) {
+        mid  = (left+right) / 2;
+        merge(arr, left, mid);
+        merge(arr, mid+1, right);
+        merge_list(arr, left, mid, right);
+    }
+}
+
+void quick(int arr[], int start, int end) {
+    if(start >= end) {
+        return;
+    }
+    
+    int pivot = start ;
+    int i = pivot + 1;
+    int j = end;
+    int temp;
+
+    while(i <= j) {
+        while(i <= end && arr[i] <= arr[pivot]) {
+            i++;
+        }
+        while(j > start && arr[j] >= arr[pivot]) {
+            j--;
+        }
+
+        if(i > j) { // cross
+            temp = arr[j];
+            arr[j] = arr[pivot];
+            arr[pivot] = temp;
+        }
+        else {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    quick(arr, start, j-1);
+    quick(arr, j+1, end);
 }
