1dac1374198e3c354d0a9ff4bfdeabf9aa0e1ebc
devsungyeon <sungyeonlee1103@gmail.com>
2021-03-06 17:47:10 +0900

    heap done

--- sorting.c
+++ sorting.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #define MAX_SIZE 10
+#define parent(x) (x-1)/2
 int sorted[MAX_SIZE];
 
 void selection(int arr[], int n);
@@ -7,6 +8,7 @@ void insertion(int arr[], int n);
 void bubble(int arr[], int n);
 void merge(int arr[], int left, int right);
 void quick(int arr[], int start, int end);
+void heap_sort(int arr[], int n);
 
 int main() {
     int n = MAX_SIZE;
@@ -55,6 +57,15 @@ int main() {
     printf("\n");
     quick(list, 0, n-1);
     printf("quick after : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+
+    for(i = 0 ; i < n ; i++ ) list[i] = n-i;
+    printf("heap before : ");
+    for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
+    printf("\n");
+    heap_sort(list, n);
+    printf("heap after : ");
     for(i = 0 ; i < n ; i++ ) printf("%d ", list[i]);
     printf("\n");
 
@@ -166,4 +177,34 @@ void quick(int arr[], int start, int end) {
     }
     quick(arr, start, j-1);
     quick(arr, j+1, end);
+}
+
+void heapify(int arr[], int n) {
+    for(int i=1; i<n; i++) {
+        int child = i;
+        while(child > 0){
+            int root = parent(child);
+            if(arr[root] < arr[child]){
+                int temp = arr[root];
+                arr[root] = arr[child];
+                arr[child] = temp;
+            }
+            child = root;
+        }
+    }
+}
+
+void heap(int arr[], int *size) {
+    int temp = arr[0];
+	arr[0] = arr[*size-1];
+	arr[*size-1] = temp;
+	--(*size);
+}
+
+void heap_sort(int arr[], int n) {
+    int size = n;
+    for(int i = 0;i<n;i++){
+        heapify(arr, size);
+        heap(arr, &size);
+    }
 }
