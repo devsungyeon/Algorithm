89b41220fa38cc1d4075a97c716b37d2bbf9d072
devsungyeon <sungyeonlee1103@gmail.com>
2021-03-17 21:24:34 +0900

    ing

---
+++ mst-kruskal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define M 20
+
+int parent[M], rank[M];
+
+typedef struct EDGE
+{
+    int u;
+    int v;
+    int w;
+}Edge;
+
+
+int main() {
+    int i,j,k,n,e,w;
+    Edge *E;
+
+    
+}
